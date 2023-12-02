0fc1cd63daa1678104b701db40ef7fba721a4ba2
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-12-02 14:11:23 +0900

    Update water.php

--- water.php
+++ water.php
@@ -1,15 +1,11 @@
 <?php
-
 $N = intval(fgets(STDIN));
-
 $m = intval($N / 5);
 $diff = $N % 5;
-
 if ($diff <= 2) {
     $result = $m * 5;
 } else {
     $result = ($m + 1) * 5;
 }
-
 echo $result . PHP_EOL;
 ?>
