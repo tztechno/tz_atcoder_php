048da1ec55441f02545ff95594e6320acb9e5ffa
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-12-06 19:03:50 +0900

    Update abc.php

--- abc.php
+++ abc.php
@@ -1,14 +1,6 @@
 <?php
-$S = trim(fgets(STDIN));
-$N = strlen($S);
-
-if ($N < 4) {
-    $S = str_pad($S, 4, "0", STR_PAD_LEFT);
-    $N = strlen($S); // Recalculate the length after padding
-}
-
+$N = trim(fgets(STDIN));
 $result;
-
 if ($N <= 125) {
     $result = 4;
 } else if ($N <= 211) {
@@ -16,6 +8,5 @@ if ($N <= 125) {
 } else {
     $result = 8;
 }
-
 echo $result . "\n";
 ?>
