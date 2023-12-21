dadea4128b49354f14fd59a4c06bb48615c30547
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-12-21 16:24:44 +0900

    Create rotation.php

---
+++ rotation.php
@@ -0,0 +1,16 @@
+#ABC181_A
+#rotation
+#input= 5
+
+
+<?php
+
+$N = intval(trim(fgets(STDIN)));
+if ($N%2==0) {
+    echo "White" . PHP_EOL;
+    }
+else{
+    echo "Black" . PHP_EOL;        
+    }
+    
+?>
